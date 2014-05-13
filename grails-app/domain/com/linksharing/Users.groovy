90af183ad11a118b75a2bed8c01667d6c36d4255
sanjeevit31 <sanjeevit31>
2014-05-13 10:46:20 +0530

    added

---
+++ grails-app/domain/com/linksharing/Users.groovy
@@ -0,0 +1,22 @@
+package com.linksharing
+
+class Users {
+    String firstName
+    String middleName
+    String lastName
+    Integer age
+    String sex
+    Integer mob
+   public void show(){
+       Services service=new Services();
+       setFirstName(service.name)
+
+   }
+
+
+    static constraints = {
+    }
+    static mapping = {
+        table('uuuuuuuuuuuuuuuuuu')
+    }
+}
---
+++ src/groovy/com/linksharing/Services.groovy
@@ -0,0 +1,11 @@
+package com.linksharing
+
+/**
+ * Created by sanjeev on 12/5/14.
+ */
+class Services {
+    String name='sanjeev kumar jha'
+    Integer id=100
+    Integer mob=8802668433
+    String email='sanjeev.jha@intelligrape.com'
+}
