e115aad9cc4b5382850e17837419f430d2b83900
sanjeevit31 <sanjeev.jha@intelligrape.com>
2014-06-05 11:11:45 +0530

    after Datasource change

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -28,6 +28,8 @@ environments {
 //            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:mysql://localhost:3306/sanjeevjha"
+            username = "root"
+            password = "igdefault"
         }
     }
     production {
